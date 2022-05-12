7269b5d12561a7f9c889db5237d895a48192780a
Triple the Boba ! <coconuttt1010@gmail.com>
2022-05-12 01:01:11 -0700

    init api res

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,39 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import request from '../routes/request';
+
+import { Configuration, OpenAIApi } from "openai";
 
 export default function Form() {
   const [inputValue, setInputValue] = useState();
+  const [responseValue, setResponseValue] = useState();
 
   const handleChange = e => {
     setInputValue(e.target.value);
   }
+  // send prompt to openAI
   const submitToAPI = (e) => {
     e.preventDefault();
-    alert("input: " + inputValue);
+    // alert("input: " + inputValue);
+    // useEffect(() => )
+    
+    // ---- OPEN AI -----
+    const configuration = new Configuration({
+      apiKey: process.env.OPENAI_API_KEY,
+    });
+    const openai = new OpenAIApi(configuration);
+    openai.createCompletion("text-curie-001", {
+      prompt: `${inputValue}`,
+      temperature: 0.5,
+      max_tokens: 64,
+      top_p: 1.0,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+    })
+    .then(res => {
+      setResponseValue(res.data.choices[0].text)
+    })
+    // ----------------------
+    
   }
 
   return (
@@ -35,6 +60,12 @@ export default function Form() {
           </div>
         </div>
       </div>
+      <p>
+        {inputValue}
+      </p>
+      <p>
+        api: {responseValue}
+      </p>
     </div>
 
   )
--- src/routes/getResponse.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import axios from 'axios';
-
-export default function getResponse() {
-  return (
-    <div>getResponse</div>
-  )
-}
