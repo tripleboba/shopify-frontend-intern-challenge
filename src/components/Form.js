f6ef3f3d1258b6c66b27c037f7d5251117f680e2
Triple the Boba ! <coconuttt1010@gmail.com>
2022-05-19 19:04:24 -0700

    add loading button when waiting for res from api

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,14 +4,25 @@ const OPENAI_KEY = process.env.REACT_APP_OPENAI_API_KEY;
 export default function Form() {
   const [inputValue, setInputValue] = useState("");
   const [responseValue, setResponseValue] = useState();
-  
-  // textarea; input value handler
+  const [isLoading, setLoading] = useState(false);
+
+  const resultsList = [
+    {
+      id: '123',
+      created: "date",
+      prompt: 'abc',
+      response: 'cba'
+    }
+  ];
+  // textarea-input-value handler
   const handleChange = e => {
     setInputValue(e.target.value);
   }
-  // form submit button handler
+
+  // form-submit-button handler
   async function submitToAPI(e) {
     e.preventDefault();
+    setLoading(true);
     const requestData = {
       prompt: `${inputValue}`,
       temperature: 0.5,
@@ -20,21 +31,39 @@ export default function Form() {
       frequency_penalty: 0.0,
       presence_penalty: 0.0,
     }
-    const response = await fetch("https://api.openai.com/v1/engines/text-curie-001/completions", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${OPENAI_KEY}`,
-      },
-      body: JSON.stringify(requestData),
-    })
-    const responseData = await response.json();
-    console.log(responseData);
-    setResponseValue(responseData.choices[0].text);
-    // setInputValue(""); // for auto clear inputValue when submitted
+    try {
+      const response = await fetch("https://api.openai.com/v1/engines/text-curie-001/completions", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${OPENAI_KEY}`,
+        },
+        body: JSON.stringify(requestData),
+      })
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+
+      const responseData = await response.json().then(setLoading(false));
+      console.log(responseData);
+      setResponseValue(responseData.choices[0].text);
+      // setInputValue(""); // for auto clear inputValue when submitted
+      addToResultList(responseData);
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   const addToResultList = (responseData) => {
+    resultsList.push(
+      {
+        id: responseData.id,
+        created: responseData.created,
+        prompt: inputValue,
+        response: responseValue
+      }
+    );
+    console.log("resutlsLits>>>"+resultsList)
   }
 
   return (
@@ -58,10 +87,9 @@ export default function Form() {
         <div className="field-body">
           <div className="field">
             <div className="control">
-              <button className="button is-dark is-outlined"
+              <button className={isLoading ? "button is-loading" : "button is-dark is-outlined"}
                 onClick={submitToAPI}
               >Submit</button>
-              {/* <button className="button is-loading">Loading</button> */}
             </div>
           </div>
         </div>
