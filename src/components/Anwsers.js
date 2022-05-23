708ebf54d2dac5cfb09f427d57ef3d31f5e749be
Triple the Boba ! <coconuttt1010@gmail.com>
2022-05-22 20:35:00 -0700

    add answers

--- src/components/Anwsers.js
+++ src/components/Anwsers.js
@@ -5,17 +5,19 @@ export default function Anwsers() {
     <div className="section">
       <p className='title is-4'>Answsers</p>
       <div className="content">
-        <p>
-          If you were to open a Shopify store, what would you sell and why (or tell us about your store if you have one!) (200 word limit)
-          Any last thoughts? (ie. proud accomplishments, cool ideas, a joke, etc.) (200 word limit)
-        </p>
-        <ul>
-          <li>PDF resume on Google Drive: <a href="https://bit.ly/39EsuNj">bit.ly/39EsuNj</a></li>
-          <li>GitHub repo of this challenge: <a href="https://github.com/tripleboba/shopify-frontend-intern-challenge">tripleboba/shopify-frontend-intern-challenge</a></li>
-          <li>OpenAI may automatically rotate my API key which will make my app unable to get the response from OpenAI API to perform its functionality.
-            <br></br>I have added this challenge visuals on GitHub repo in case it happens
+        <ol type="1">
+          <li>
+            <strong>If I were to open a Shopify store</strong>, I would open a service specializing in renting out random things in the area for short periods.
+            <br></br>The idea came from once when I went to a milk tea shop and left my glasses at home while I was more than halfway to the store.
+            While I had a hard time reading the tiny menu of the milk tea shop hanging in the back, next to me was a couple making an order.
+            The boyfriend read each item on the menu to his girlfriend for her since she couldn't see those mini words too.
+            At that moment, I thought why would noone make an app that could rent a person in the area with a 10/10 eyesight or the binoculars to read the menu and get pay for those 2-3 minutes.
+            If running a business was easy, I would create an online store offering that service.
           </li>
-        </ul>
+          <li>
+            <strong>Last thoughts?</strong> Yes. I just realized that my idea of opening this type of service is pretty bad because users need a phone to book a service. And smartphones all have the zoom in feature which people can use to read the menu. This is probably the reason why no one has opened this service.
+          </li>
+        </ol>
       </div>
     </div>
   )
